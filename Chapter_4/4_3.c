0466433f7fe8a2810b7688205297ba6e23faca93
EternalPlace <35691707+EternalPlace@users.noreply.github.com>
2019-01-12 10:21:53 +0800

    Create 4_3.c

---
+++ Chapter_4/4_3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+main()
+{
+	int x, y;
+  	printf("Enter x & y: ");
+  	scanf("%d%d", &x, &y);
+  	printf("x, y: %d %d\n", x, y);
+	if (x>y) {
+		printf("max = x = %d\n", x);
+   }
+  	else
+   	printf("max = y = %d\n", y);
+   printf("* * end * *\n");
+}
