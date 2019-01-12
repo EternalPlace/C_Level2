7288dabc78dd5486fe1e642a0908b66bbc3daaa0
EternalPlace <35691707+EternalPlace@users.noreply.github.com>
2019-01-12 10:32:07 +0800

    Update 4_4.c

--- Chapter_4/4_4.c
+++ Chapter_4/4_4.c
@@ -2,11 +2,11 @@
 
 main()
 {
-	int n;
-	printf(“input n: ”);
-	scanf(“%d”, &n);
-	if (n%3==0)
-		printf(“n = %d YES\n”, n);
-	else
-		printf(“n = %d NO\n”, n);
+    int n;
+    printf(“input n: ”);
+    scanf(“%d”, &n);
+    if (n%3==0)
+        printf(“n = %d YES\n”, n);
+    else
+        printf(“n = %d NO\n”, n);
 }
