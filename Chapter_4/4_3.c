ae46f933ce47f555636ae6923a9452876b938be7
EternalPlace <35691707+EternalPlace@users.noreply.github.com>
2019-01-12 10:25:40 +0800

    Update 4_3.c

--- Chapter_4/4_3.c
+++ Chapter_4/4_3.c
@@ -2,14 +2,14 @@
 
 main()
 {
-	int x, y;
-  	printf("Enter x & y: ");
-  	scanf("%d%d", &x, &y);
-  	printf("x, y: %d %d\n", x, y);
-	if (x>y) {
-		printf("max = x = %d\n", x);
+    int x, y;
+    printf("Enter x & y: ");
+    scanf("%d%d", &x, &y);
+    printf("x, y: %d %d\n", x, y);
+    if (x>y) {
+        printf("max = x = %d\n", x);
     }
-  	else
-   	printf("max = y = %d\n", y);
+    else
+    printf("max = y = %d\n", y);
     printf("* * end * *\n");
 }
