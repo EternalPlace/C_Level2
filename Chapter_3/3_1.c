127b71eab1f1d179645976749c392d3b4978715a
EternalPlace <35691707+EternalPlace@users.noreply.github.com>
2019-01-12 09:43:22 +0800

    Update and rename 3.1.c to 3_1.c

---
+++ Chapter_3/3_1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+main()
+{
+    int x, y, t;
+    printf("Enter x & y: \n");
+    scanf("%d%d", &x, &y);
+    printf("x = %d y = %d\n", x, y);
+    t = x;
+    x = y;
+    y = t;
+    printf("x = %d y = %d\n", x, y);
+}
