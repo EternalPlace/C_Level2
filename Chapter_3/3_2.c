6c0d5473f80d8c4ca9394f1cdcd32011b62d5c5f
EternalPlace <35691707+EternalPlace@users.noreply.github.com>
2019-01-12 09:53:49 +0800

    Update 3_2.c

--- Chapter_3/3_2.c
+++ Chapter_3/3_2.c
@@ -1,3 +1,14 @@
 #include <stdio.h>
 
 main()
+{
+    double x;
+    printf("Enter x: \n");
+    scanf("%lf", &x);
+    printf("(1)x = %f\n", x);
+    x = x * 100;
+    x = x + 0.5;
+    x = (int)x;
+    x = x / 100;
+    printf("(2)x = %f\n", x);
+}
