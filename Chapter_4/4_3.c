bf9a474fd96523f0bbf76fb9d4e0aaf01b2e1067
EternalPlace <35691707+EternalPlace@users.noreply.github.com>
2019-01-12 10:23:30 +0800

    Update 4_3.c

--- Chapter_4/4_3.c
+++ Chapter_4/4_3.c
@@ -8,8 +8,8 @@ main()
   	printf("x, y: %d %d\n", x, y);
 	if (x>y) {
 		printf("max = x = %d\n", x);
-   }
+    }
   	else
    	printf("max = y = %d\n", y);
-   printf("* * end * *\n");
+    printf("* * end * *\n");
 }
